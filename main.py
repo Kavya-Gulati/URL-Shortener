601cf892257d7b4a06a581dada26819dcedc7c48
Kavya Gulati <kgulati2209@gmail.com>
2026-09-18 13:46:45 +0530

    added url validity check function

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import hashlib, argparse, json
+from urllib.parse import urlparse #To check for valid URLS
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--shorten',help='Shortens provided URL')
+parser.add_argument('-s', '--shorten',help='Shortens provided URL',nargs='?') #nargs = '?' allows the argument to be blank without throwing an exception.
 
+def CheckValidity(url):
+    try:
+        result = urlparse(url)
+        print(result.netloc)
+
+        if result.scheme not in ('https', 'http'):
+            return False
+        
+        if not result.netloc:
+            return False
+
+        return True
+    except:
+        return False
+    
 args = parser.parse_args()
 print(args)
-print(type(args.shorten))
-if args.shorten is not None:
-    print('testing true')
+if args.shorten is None:
+    print('error: No URL provided')
+else:
+    validity = CheckValidity(args.shorten)
+    if validity == False:
+        print("error: Invalid URL")
