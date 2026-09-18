fa5e84e3688a23190404f963cacc0b3dced25d3c
Kavya Gulati <kgulati2209@gmail.com>
2026-09-18 14:46:49 +0530

    finished url shorten command

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import hashlib, argparse, json
 from urllib.parse import urlparse #To check for valid URLS
-parser = argparse.ArgumentParser(description='URL Shortener')
 
-parser.add_argument('-s', '--shorten',help='Shortens provided URL',nargs='?') #nargs = '?' allows the argument to be blank without throwing an exception.
+parser = argparse.ArgumentParser(description='URL Shortener')
+subparsers = parser.add_subparsers(dest='command', required=True)
+
+parser_shorten = subparsers.add_parser('shorten', help = 'shortens a URL')
+parser_shorten.add_argument('URL', help = 'URL to be shortened', nargs = "?")
+parser_shorten.add_argument('--alias', help = 'provide an alias(optional)', default= None)
 
 def CheckValidity(url):
     try:
@@ -19,27 +23,48 @@ def CheckValidity(url):
     except:
         return False
 
-def ShortenURL(url):
-    h = hashlib.new('md5') #It generates the shortest hash(though it's not as secure as other algorithms)
-    h.update(url.encode())
-    url_hash = h.hexdigest()
+def ShortenURL(args):
+    url = args.URL
+    alias = args.alias
+
+    if url is None:
+        print('error: No URL provided')
+        return
+
+    else:
+        validity = CheckValidity(url)
+
+        if validity == False:
+            print("error: Invalid URL")
+            return
+
+        else:
+            print('Valid URL')
+
+            data = {}
+            try:
+                with open('data.json','r') as f:
+                    data = json.load(f)
+
+            except:
+                data = {}
+
+            if alias is None: 
+                h = hashlib.new('md5') #It generates the shortest hash(though it's not as secure as other algorithms)
+                h.update(url.encode())
+                url_hash = h.hexdigest()
+                data[url] = url_hash
+            
+            else:
+                data[url] = alias
+
+            with open('data.json','w') as f:
+                json.dump(data,f)
+            
     
-    data = {url:url_hash}
-    
-    with open('data.json','a') as f:
-        json.dump(data,f)
     
 args = parser.parse_args()
 print(args)
 
-if args.shorten is None:
-    print('error: No URL provided')
-
-else:
-    validity = CheckValidity(args.shorten)
-
-    if validity == False:
-        print("error: Invalid URL")
-
-    else:
-        ShortenURL(args.shorten)
+if args.command == 'shorten':
+    ShortenURL(args)
