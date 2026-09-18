dc42bf873146bbbf81069ef6e39ec012b234616e
Kavya Gulati <kgulati2209@gmail.com>
2026-09-18 14:08:22 +0530

    added shorten url function

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import hashlib, argparse, json
 from urllib.parse import urlparse #To check for valid URLS
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='URL Shortener')
+
 parser.add_argument('-s', '--shorten',help='Shortens provided URL',nargs='?') #nargs = '?' allows the argument to be blank without throwing an exception.
 
 def CheckValidity(url):
     try:
         result = urlparse(url)
-        print(result.netloc)
 
         if result.scheme not in ('https', 'http'):
             return False
@@ -15,14 +15,31 @@ def CheckValidity(url):
             return False
 
         return True
+    
     except:
         return False
+
+def ShortenURL(url):
+    h = hashlib.new('md5') #It generates the shortest hash(though it's not as secure as other algorithms)
+    h.update(url.encode())
+    url_hash = h.hexdigest()
+    
+    data = {url:url_hash}
+    
+    with open('data.json','a') as f:
+        json.dump(data,f)
     
 args = parser.parse_args()
 print(args)
+
 if args.shorten is None:
     print('error: No URL provided')
+
 else:
     validity = CheckValidity(args.shorten)
+
     if validity == False:
         print("error: Invalid URL")
+
+    else:
+        ShortenURL(args.shorten)
